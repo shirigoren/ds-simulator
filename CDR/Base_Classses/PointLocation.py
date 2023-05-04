c3129c86aa58312cb1321c9fffd9d1258d192205
raanan <raanany85@gmail.com>
2023-05-04 17:04:41 +0300

    update and improve PointLocation

--- CDR/Base_Classses/PointLocation.py
+++ CDR/Base_Classses/PointLocation.py
@@ -1,3 +1,6 @@
+import math
+
+
 class PointLocation(object):
     def __init__(self, lat, long):
         self.lat = lat
@@ -5,28 +8,38 @@ class PointLocation(object):
         self.geo_coordinate = (self.lat, self.long)
         self.print_debug = False
 
-    # ##------------------------------Equality and str methods -------------------------------------##
-
+    # Equality method
     def __eq__(self, other):
-        if type(other) is PointLocation:
-            return self.lat == other.lat and self.long == other.long
-        print("type ERROR")
-        return False
+        if not isinstance(other, PointLocation):
+            return NotImplemented
+        return self.geo_coordinate == other.geo_coordinate
 
+    # str & repr method
     def __str__(self):
         return f"-- Point Location is -- \n   Latitude:{self.lat} \n   Longitude:{self.long} \n"
 
+    def __repr__(self):
+        return str(self)
 
-# TESTS
+    # distance from an other PointLocation
+    def distance(self, other):
+        dx = self.lat - other.lat
+        dy = self.long - other.long
+        return round(math.sqrt(dx ** 2 + dy ** 2), 2)
+
+
+# # TESTS
 # p = PointLocation(5, 2)
 # print(p)
-
-t = PointLocation(2, 5)
-print(t)
+#
+# t = PointLocation(2, 5)
+# print(t)
 #
 # g = PointLocation(3, 5)
-g = [3, 5]
-print(g)
+# print(g)
 #
 # print(p == t)
-print(t < g)
+# print(p == p)
+#
+# print(p.distance(t))
+
